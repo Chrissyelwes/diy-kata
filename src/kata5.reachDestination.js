00ef328f0e22c61fdf627f1de241265118caf6b2
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-02-27 12:10:02 +0000

    finished reachDestination kata

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,8 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const estimatedArrival = (distance / speed)
+  const roundHalf = Math.round((estimatedArrival * 2) / 2);
+
+  return `I should be there in ${roundHalf} hours`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
+/* eslint-disable prettier/prettier */
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+   expect(reachDestination(60, 30)).toEqual('I should be there in 2 hours');
+   expect(reachDestination(95, 20)).toEqual('I should be there in 5 hours');
+  });
 });
