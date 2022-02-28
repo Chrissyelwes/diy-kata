2662e29ffc811f9feedb7d5be10210d896f98cfc
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-02-28 10:53:27 +0000

    joinNames kata test done

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,16 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  if (namesObj.length > 1) {
+    return `${otherNames(namesObj)} & ${namesObj[namesObj.length - 1].name}`;
+  } else if (namesObj.length === 1) {
+    return namesObj[0].name;
+  }
+  return "";
+};
+function otherNames(array) {
+  return array
+    .splice(0, array.length - 1)
+    .map(person => person.name)
+    .join(" , ");
+}
 
 module.exports = joinNames;
